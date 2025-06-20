5527c3ea976d96d37661df47e837f278c049eeed
Kumar Priyanshu <az.priyanshu.rockstar@gmail.com>
2025-06-20 23:58:42 +0530

    chore: added handler

--- cmd/main.go
+++ cmd/main.go
@@ -4,14 +4,15 @@ import (
 	"log"
 
 	"github.com/gofiber/fiber/v2"
+	"github.com/kpriyanshu2003/url-shortener/internal/controller"
 )
 
 func main() {
 	app := fiber.New()
+	app.Static("/", "./public")
 
-	// Routes
-	// app.Post("/shorten", handler.ShortenURL)
-	// app.Get("/:code", handler.Redirect)
+	app.Post("/", controller.ShortenURL)
+	app.Get("/:code", controller.Redirect)
 
-	log.Fatal(app.Listen(":3000"))
+	log.Fatal(app.Listen(":3300"))
 }
---
+++ internal/controller/url.go
@@ -0,0 +1,50 @@
+package controller
+
+import (
+	"net/http"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/kpriyanshu2003/url-shortener/internal/model"
+)
+
+var urlStore = make(map[string]string) // temp store
+
+func ShortenURL(c *fiber.Ctx) error {
+	var payload model.URLMapping
+	if err := c.BodyParser(&payload); err != nil {
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{"error": "Invalid input"})
+	}
+
+	if payload.URL == "" {
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+			"error": "Iriginal URL is required",
+		})
+	}
+	if payload.Code == "" {
+		// Generate a random code if not provided
+		payload.Code = "short" // This should be replaced with a proper random code generation logic
+	}
+
+	// DB interaction
+	urlStore[payload.Code] = payload.URL
+
+	return c.Status(http.StatusCreated).JSON(fiber.Map{
+		"short_url":    "/" + payload.Code,
+		"original_url": payload.URL,
+	})
+}
+
+func Redirect(c *fiber.Ctx) error {
+	code := c.Params("code")
+
+	// DB interaction
+	originalURL, found := urlStore[code]
+	if !found {
+		return c.Status(fiber.StatusNotFound).SendString("Short URL not found")
+	}
+	return c.Redirect(originalURL, http.StatusTemporaryRedirect)
+}
+
+func RenderHomePage(c *fiber.Ctx) error {
+	return c.Render("index", fiber.Map{"Title": "URL Shortener"})
+}
